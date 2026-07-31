e157e7a809e3a5a908d1fb73557393705a44c80b
kimal2 <18127672512@163.com>
2026-07-31 19:31:18 +0800

    有刷电机能动

--- My_Drivers/motor.c
+++ My_Drivers/motor.c
@@ -4,30 +4,12 @@
 /* Uncomment to exchange the logical left and right motor commands. */
 /* #define MOTOR_SWAP_LEFT_RIGHT */
 
-#define MOTOR_COMMAND_MAX       1000L
+#define PWM_PERIOD              100L
 
 #define MOTOR_LEFT_IN1_CHANNEL  TIM_CHANNEL_2  /* PE11, AIN1 */
 #define MOTOR_LEFT_IN2_CHANNEL  TIM_CHANNEL_1  /* PE9,  AIN2 */
 #define MOTOR_RIGHT_IN1_CHANNEL TIM_CHANNEL_4  /* PE14, BIN1 */
 #define MOTOR_RIGHT_IN2_CHANNEL TIM_CHANNEL_3  /* PE13, BIN2 */
-
-static uint32_t Motor_GetPeriod(void)
-{
-    return __HAL_TIM_GET_AUTORELOAD(&htim1) + 1U;
-}
-
-static uint32_t Motor_CommandToPulse(int32_t command)
-{
-    uint32_t magnitude;
-
-    if (command < 0) {
-        magnitude = (uint32_t)(-command);
-    } else {
-        magnitude = (uint32_t)command;
-    }
-
-    return (magnitude * Motor_GetPeriod()) / (uint32_t)MOTOR_COMMAND_MAX;
-}
 
 void Motor_Init(void)
 {
@@ -44,66 +26,63 @@ void Motor_Init(void)
     Motor_Sleep();
 }
 
-void Motor_Set_PWM(int32_t PWM_L, int32_t PWM_R)
+void Motor_Set_PWM(int32_t PWM_L,int32_t PWM_R)
 {
-    uint32_t left_pulse;
-    uint32_t right_pulse;
 
-#ifdef MOTOR_SWAP_LEFT_RIGHT
+    /* 根据机械安装方向选择是否交换左右电机指令。 */
+    #ifdef MOTOR_SWAP_LEFT_RIGHT
     int32_t temp;
-
     temp = PWM_L;
     PWM_L = PWM_R;
     PWM_R = temp;
-#endif
+    #endif
 
-    if (PWM_L > MOTOR_COMMAND_MAX) {
-        PWM_L = MOTOR_COMMAND_MAX;
-    } else if (PWM_L < -MOTOR_COMMAND_MAX) {
-        PWM_L = -MOTOR_COMMAND_MAX;
+    /* 将左右电机指令限制在定时器允许的范围内。 */
+    if(PWM_L>PWM_PERIOD)        PWM_L = PWM_PERIOD;
+    else if(PWM_L<-PWM_PERIOD)  PWM_L = -PWM_PERIOD;
+
+    if(PWM_R>PWM_PERIOD)        PWM_R = PWM_PERIOD;
+    else if(PWM_R<-PWM_PERIOD)  PWM_R = -PWM_PERIOD;
+
+
+    int32_t CCR_Left;
+    int32_t CCR_Right;
+
+    /* 设置左电机 H 桥两路 PWM 比较值。 */
+    if(PWM_L>0)
+    {
+        CCR_Left = PWM_PERIOD - PWM_L;
+        __HAL_TIM_SET_COMPARE(&htim1, MOTOR_LEFT_IN1_CHANNEL, PWM_PERIOD);
+        __HAL_TIM_SET_COMPARE(&htim1, MOTOR_LEFT_IN2_CHANNEL, CCR_Left);
+    }
+    else
+    {
+        CCR_Left = PWM_PERIOD + PWM_L;
+        __HAL_TIM_SET_COMPARE(&htim1, MOTOR_LEFT_IN1_CHANNEL, CCR_Left);
+        __HAL_TIM_SET_COMPARE(&htim1, MOTOR_LEFT_IN2_CHANNEL, PWM_PERIOD);
     }
 
-    if (PWM_R > MOTOR_COMMAND_MAX) {
-        PWM_R = MOTOR_COMMAND_MAX;
-    } else if (PWM_R < -MOTOR_COMMAND_MAX) {
-        PWM_R = -MOTOR_COMMAND_MAX;
+    /* 设置右电机 H 桥两路 PWM 比较值。 */
+    if(PWM_R>0)
+    {
+        CCR_Right = PWM_PERIOD - PWM_R;
+        __HAL_TIM_SET_COMPARE(&htim1, MOTOR_RIGHT_IN1_CHANNEL, CCR_Right);
+        __HAL_TIM_SET_COMPARE(&htim1, MOTOR_RIGHT_IN2_CHANNEL, PWM_PERIOD);
     }
-
-    left_pulse = Motor_CommandToPulse(PWM_L);
-    right_pulse = Motor_CommandToPulse(PWM_R);
-
-    if (PWM_L > 0) {
-        __HAL_TIM_SET_COMPARE(&htim1, MOTOR_LEFT_IN1_CHANNEL, left_pulse);
-        __HAL_TIM_SET_COMPARE(&htim1, MOTOR_LEFT_IN2_CHANNEL, 0U);
-    } else if (PWM_L < 0) {
-        __HAL_TIM_SET_COMPARE(&htim1, MOTOR_LEFT_IN1_CHANNEL, 0U);
-        __HAL_TIM_SET_COMPARE(&htim1, MOTOR_LEFT_IN2_CHANNEL, left_pulse);
-    } else {
-        __HAL_TIM_SET_COMPARE(&htim1, MOTOR_LEFT_IN1_CHANNEL, 0U);
-        __HAL_TIM_SET_COMPARE(&htim1, MOTOR_LEFT_IN2_CHANNEL, 0U);
-    }
-
-    if (PWM_R > 0) {
-        __HAL_TIM_SET_COMPARE(&htim1, MOTOR_RIGHT_IN1_CHANNEL, right_pulse);
-        __HAL_TIM_SET_COMPARE(&htim1, MOTOR_RIGHT_IN2_CHANNEL, 0U);
-    } else if (PWM_R < 0) {
-        __HAL_TIM_SET_COMPARE(&htim1, MOTOR_RIGHT_IN1_CHANNEL, 0U);
-        __HAL_TIM_SET_COMPARE(&htim1, MOTOR_RIGHT_IN2_CHANNEL, right_pulse);
-    } else {
-        __HAL_TIM_SET_COMPARE(&htim1, MOTOR_RIGHT_IN1_CHANNEL, 0U);
-        __HAL_TIM_SET_COMPARE(&htim1, MOTOR_RIGHT_IN2_CHANNEL, 0U);
+    else
+    {
+        CCR_Right = PWM_PERIOD + PWM_R;
+        __HAL_TIM_SET_COMPARE(&htim1, MOTOR_RIGHT_IN1_CHANNEL, PWM_PERIOD);
+        __HAL_TIM_SET_COMPARE(&htim1, MOTOR_RIGHT_IN2_CHANNEL, CCR_Right);
     }
 }
 
 void Motor_Stop(void)
 {
-    uint32_t period;
-
-    period = Motor_GetPeriod();
-    __HAL_TIM_SET_COMPARE(&htim1, MOTOR_LEFT_IN1_CHANNEL, period);
-    __HAL_TIM_SET_COMPARE(&htim1, MOTOR_LEFT_IN2_CHANNEL, period);
-    __HAL_TIM_SET_COMPARE(&htim1, MOTOR_RIGHT_IN1_CHANNEL, period);
-    __HAL_TIM_SET_COMPARE(&htim1, MOTOR_RIGHT_IN2_CHANNEL, period);
+    __HAL_TIM_SET_COMPARE(&htim1, MOTOR_LEFT_IN1_CHANNEL, PWM_PERIOD);
+    __HAL_TIM_SET_COMPARE(&htim1, MOTOR_LEFT_IN2_CHANNEL, PWM_PERIOD);
+    __HAL_TIM_SET_COMPARE(&htim1, MOTOR_RIGHT_IN1_CHANNEL, PWM_PERIOD);
+    __HAL_TIM_SET_COMPARE(&htim1, MOTOR_RIGHT_IN2_CHANNEL, PWM_PERIOD);
 }
 
 void Motor_Sleep(void)
